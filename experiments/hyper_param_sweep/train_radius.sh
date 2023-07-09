78f7679873a551663b3342082d274d9cde335b8c
oubino <oliver.umney@hotmail.co.uk>
2023-07-09 13:23:14 +0100

    jobs to run on arc

---
+++ experiments/hyper_param_sweep/train_radius.sh
@@ -0,0 +1,16 @@
+#Python train script with modified radius
+
+#Run with current environment (-V) and in the current directory (-cwd)
+#$ -V -cwd
+
+#Request some time- min 15 mins - max 48 hours
+#$ -l h_rt=2:10:00
+
+# GPU/CPU request
+#$ -l coproc_v100=1
+
+#Get email at start and end of the job
+#$ -m be
+
+#Now run the job
+python locpix_points/src/locpix_points/scripts/train.py -i /nobackup/scou/output/nieves/expt1 -c locpix_points/src/locpix_points/experiments/hyper_param_sweep/radius.yaml
