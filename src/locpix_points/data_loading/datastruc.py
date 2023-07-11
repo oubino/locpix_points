0b32953f8b2baaa4cfeb098d4b15dba56fb0f29f
oubino <oliver.umney@hotmail.co.uk>
2023-07-11 12:02:19 +0100

    fix up bugs

--- src/locpix_points/data_loading/datastruc.py
+++ src/locpix_points/data_loading/datastruc.py
@@ -123,9 +123,6 @@ class SMLMDataset(Dataset):
 
             output_transforms = transforms.Compose(output_transforms)
 
-            print(output_transforms)
-            print('here')
-
             super().__init__(None, output_transforms, pre_transform, pre_filter)
 
     @property
