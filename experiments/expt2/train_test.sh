f086a6e33778991e326c1a65d1d7f5dd57d8e98a
oubino <oliver.umney@hotmail.co.uk>
2023-07-14 15:33:47 +0100

    create experiment 2

---
+++ experiments/expt2/train_test.sh
@@ -0,0 +1,19 @@
+#Python train script with modified k
+
+#Run with current environment (-V) and in the current directory (-cwd)
+#$ -V -cwd
+
+#Request some time- min 15 mins - max 48 hours
+#$ -l h_rt=00:15:00
+
+# GPU/CPU request
+#$ -l coproc_v100=1
+
+#Get email at start and end of the job
+#$ -m be
+
+# only one job at a time
+#$ -tc 1
+
+#Now run the job
+python locpix_points/src/locpix_points/scripts/train.py -i /nobackup/scou/output/nieves/expt2 -c locpix_points/experiments/expt/test.yaml
