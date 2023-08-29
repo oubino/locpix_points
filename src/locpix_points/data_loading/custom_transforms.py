2f8e379fcb1f95bd8430b34016f1def71b76371c
oubino <oliver.umney@hotmail.co.uk>
2023-08-29 18:02:29 +0100

    change subgraph

--- src/locpix_points/data_loading/custom_transforms.py
+++ src/locpix_points/data_loading/custom_transforms.py
@@ -4,13 +4,14 @@ from torch_geometric.data import Data
 from torch_geometric.data.datapipes import functional_transform
 from torch_geometric.transforms import BaseTransform
 import torch_geometric.transforms as T
-from torch_geometric.nn import (
-    radius,
-)
-from torch_geometric.utils import subgraph
+#from torch_geometric.nn import (
+#    radius,
+#)
+#from torch_geometric.utils import subgraph
 import numpy as np
-import torch
-import sys
+#import torch
+#import sys
+import warnings
 
 # had to change base code as basetransform not implemented yet for me 
 @functional_transform('subsample')
@@ -36,17 +37,34 @@ class Subsample(BaseTransform):
 
         print('number nodes pre')
         print(data.num_nodes)
+        print(data.x)
+        print(data.pos)
+        print(data.y)
 
         data_min_x = data.pos[:,0] > data.pos[idx[0]][0] - self.x/2
         data_max_x = data.pos[:,0] < data.pos[idx[0]][0] + self.x/2
         data_min_y = data.pos[:,1] > data.pos[idx[0]][1] - self.y/2
         data_max_y = data.pos[:,1] < data.pos[idx[0]][1] + self.y/2
+        
+        indices = data_min_x&data_max_x&data_min_y&data_max_y
+        
+        warnings.warn('Not implemented for heterogeneous')
 
-        data.pos = data.pos[data_min_x&data_max_x&data_min_y&data_max_y]
-        data.x = data.x[data_min_x&data_max_x&data_min_y&data_max_y]
+        if data.edge_index is not None:
+            raise ValueError('Not sure if below works')
+            data = data.subgraph(indices)
+        if data.edge_attr is not None:
+            raise ValueError('Not implemented')
+        if data.edge_index is None and data.edge_attr is None:
+            data.x = data.x[indices]
+            data.pos = data.pos[indices]
+            data.y = data.y[indices]
 
         print('number nodes post')
         print(data.num_nodes)
+        print(data.x)
+        print(data.pos)
+        print(data.y)
     
         return data
 
