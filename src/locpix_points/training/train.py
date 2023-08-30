823aabf9ab4e8decb6c9772e27adc54ad91c978c
oubino <oliver.umney@hotmail.co.uk>
2023-08-30 09:44:08 +0100

    remove prints and put on gpu for longer

--- src/locpix_points/training/train.py
+++ src/locpix_points/training/train.py
@@ -61,7 +61,6 @@ def train_loop(
         num_val_node = 0
 
         # training data
-        print('----- Training ------')
         model.train()
         for index, data in enumerate(train_loader):
 
@@ -92,7 +91,6 @@ def train_loop(
         # val data
         # TODO: make sure torch.no_grad() somewhere
         # make sure model in eval mode
-        print('----- Validation ------')
         model.eval()
         for index, data in enumerate(val_loader):
             with torch.no_grad():
