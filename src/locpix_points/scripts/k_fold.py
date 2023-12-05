0fe6140f6a1c9cb9db9901529a86018beeac03e4
Oliver (linux) <scou@leeds.ac.uk>
2023-12-05 17:31:58 +0000

    don't remove processed files at end of k-fold training

--- src/locpix_points/scripts/k_fold.py
+++ src/locpix_points/scripts/k_fold.py
@@ -171,31 +171,31 @@ def main(argv=None):
             ]
         )
 
-        print('Cleaning up')
+        #print('Cleaning up')
 
         # clean up process folder check it first during debugging
         #keep_files = ['file_map.csv', 'pre_filter.pt', 'pre_transform.pt']
-        keep_files = []
-        train_files = os.listdir(f'{args.project_directory}/processed/fold_{index}/train')
-        val_files = os.listdir(f'{args.project_directory}/processed/fold_{index}/val')
-        test_files = os.listdir(f'{args.project_directory}/processed/fold_{index}/test')
-        # get list of folders to delete
-        train_files = [os.path.join(f'{args.project_directory}/processed/fold_{index}/train', i) for i in train_files if i not in keep_files]
-        val_files = [os.path.join(f'{args.project_directory}/processed/fold_{index}/val', i) for i in val_files if i not in keep_files]
-        test_files = [os.path.join(f'{args.project_directory}/processed/fold_{index}/test', i) for i in test_files if i not in keep_files]
+        #keep_files = []
+        #train_files = os.listdir(f'{args.project_directory}/processed/fold_{index}/train')
+        #val_files = os.listdir(f'{args.project_directory}/processed/fold_{index}/val')
+        #test_files = os.listdir(f'{args.project_directory}/processed/fold_{index}/test')
+        ## get list of folders to delete
+        #train_files = [os.path.join(f'{args.project_directory}/processed/fold_{index}/train', i) for i in train_files if i not in keep_files]
+        #val_files = [os.path.join(f'{args.project_directory}/processed/fold_{index}/val', i) for i in val_files if i not in keep_files]
+        #test_files = [os.path.join(f'{args.project_directory}/processed/fold_{index}/test', i) for i in test_files if i not in keep_files]
 
-        for file in train_files:
-            os.remove(file)
-        for file in val_files:
-            os.remove(file)
-        for file in test_files:
-            os.remove(file)
+        #for file in train_files:
+        #    os.remove(file)
+        #for file in val_files:
+        #    os.remove(file)
+        #for file in test_files:
+        #    os.remove(file)
         
         # remove directories
-        os.rmdir(f'{args.project_directory}/processed/fold_{index}/train')
-        os.rmdir(f'{args.project_directory}/processed/fold_{index}/val')
-        os.rmdir(f'{args.project_directory}/processed/fold_{index}/test')
-        os.rmdir(f'{args.project_directory}/processed/fold_{index}')
+        #os.rmdir(f'{args.project_directory}/processed/fold_{index}/train')
+        #os.rmdir(f'{args.project_directory}/processed/fold_{index}/val')
+        #os.rmdir(f'{args.project_directory}/processed/fold_{index}/test')
+        #os.rmdir(f'{args.project_directory}/processed/fold_{index}')
 
     # save config file to folder and wandb
     yaml_save_loc = os.path.join(project_directory, f"k_fold.yaml")
