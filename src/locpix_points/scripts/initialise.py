285d370be48f7c735437e8f1639152db528faf5e
Oliver (linux) <scou@leeds.ac.uk>
2024-01-11 13:59:32 +0000

    now initialiase can allow copying of preprocessed files from another task

--- src/locpix_points/scripts/initialise.py
+++ src/locpix_points/scripts/initialise.py
@@ -48,11 +48,6 @@ def main():
         "init_time": time.gmtime(time.time()),
     }
 
-    # save metadata
-    metadata_path = os.path.join(project_directory, "metadata.json")
-    with open(metadata_path, "w") as outfile:
-        json.dump(metadata, outfile)
-
     # Copy template/config
     dir = files("locpix_points.template.config")
     dest = os.path.join(project_directory, "config")
@@ -66,6 +61,37 @@ def main():
     iterdir = dir.iterdir()
     for file in iterdir:
         shutil.copy(file, dest)
+
+    # Copy preprocessed files from another task
+    copy_preprocessed = input(
+        "Would you like to copy preprocessed files from another folder - you must type yes? "
+    )
+    if copy_preprocessed == "yes":
+        folder_loc = input("Location of the project folder: ")
+
+        # copy preprocessed folder
+        src = os.path.join(folder_loc, "preprocessed")
+        dest = os.path.join(project_directory, "preprocessed")
+        shutil.copytree(src, dest)
+
+        # copy preprocess.yaml
+        src = os.path.join(folder_loc, "preprocess.yaml")
+        shutil.copy(src, project_directory)
+
+        # add relevant metadata
+        metadata_path = os.path.join(folder_loc, "metadata.json")
+        with open(
+            metadata_path,
+        ) as file:
+            other_metadata = json.load(file)
+            # copy relevant metadata across
+            metadata["preprocess.py"] = other_metadata["preprocess.py"]
+            metadata["gt_label_map"] = other_metadata["gt_label_map"]
+
+    # save metadata
+    metadata_path = os.path.join(project_directory, "metadata.json")
+    with open(metadata_path, "w") as outfile:
+        json.dump(metadata, outfile)
 
 
 if __name__ == "__main__":
