9409f74dcf12349311388eb617d46d381b2dbf83
Oliver (linux) <scou@leeds.ac.uk>
2024-03-15 16:12:42 +0000

    Add back in removing processed files after finished fold

--- src/locpix_points/scripts/k_fold.py
+++ src/locpix_points/scripts/k_fold.py
@@ -264,25 +264,39 @@ def main(argv=None):
 
         wandb.finish()
 
-        # print('Cleaning up')
+        print("Cleaning up")
 
         # clean up process folder check it first during debugging
-        # keep_files = ['file_map.csv', 'pre_filter.pt', 'pre_transform.pt']
+        keep_files = ["file_map.csv", "pre_filter.pt", "pre_transform.pt"]
         # keep_files = []
-        # train_files = os.listdir(f'{args.project_directory}/processed/fold_{index}/train')
-        # val_files = os.listdir(f'{args.project_directory}/processed/fold_{index}/val')
-        # test_files = os.listdir(f'{args.project_directory}/processed/fold_{index}/test')
+        train_files = os.listdir(
+            f"{args.project_directory}/processed/fold_{index}/train"
+        )
+        val_files = os.listdir(f"{args.project_directory}/processed/fold_{index}/val")
+        test_files = os.listdir(f"{args.project_directory}/processed/fold_{index}/test")
         # get list of folders to delete
-        # train_files = [os.path.join(f'{args.project_directory}/processed/fold_{index}/train', i) for i in train_files if i not in keep_files]
-        # val_files = [os.path.join(f'{args.project_directory}/processed/fold_{index}/val', i) for i in val_files if i not in keep_files]
-        # test_files = [os.path.join(f'{args.project_directory}/processed/fold_{index}/test', i) for i in test_files if i not in keep_files]
+        train_files = [
+            os.path.join(f"{args.project_directory}/processed/fold_{index}/train", i)
+            for i in train_files
+            if i not in keep_files
+        ]
+        val_files = [
+            os.path.join(f"{args.project_directory}/processed/fold_{index}/val", i)
+            for i in val_files
+            if i not in keep_files
+        ]
+        test_files = [
+            os.path.join(f"{args.project_directory}/processed/fold_{index}/test", i)
+            for i in test_files
+            if i not in keep_files
+        ]
 
-        # for file in train_files:
-        #    os.remove(file)
-        # for file in val_files:
-        #    os.remove(file)
-        # for file in test_files:
-        #    os.remove(file)
+        for file in train_files:
+            os.remove(file)
+        for file in val_files:
+            os.remove(file)
+        for file in test_files:
+            os.remove(file)
 
         # remove directories
         # os.rmdir(f'{args.project_directory}/processed/fold_{index}/train')
